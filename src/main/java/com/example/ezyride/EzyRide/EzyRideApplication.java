e6d5aaf842736dfdd584bfe5f3a99e69be257a39
Anuj Jain <jainanuj.work@gmail.com>
2024-08-09 22:17:05 +0530

    Initial commit

---
+++ src/main/java/com/example/ezyride/EzyRide/EzyRideApplication.java
@@ -0,0 +1,13 @@
+package com.example.ezyride.EzyRide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzyRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzyRideApplication.class, args);
+	}
+
+}
