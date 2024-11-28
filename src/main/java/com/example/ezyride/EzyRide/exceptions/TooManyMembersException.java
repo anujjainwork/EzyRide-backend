452bc3b67761211afe5afa501bb3e2f205cbb71b
anujjainwork <jainanuj.work@gmail.com>
2024-11-28 20:20:46 +0530

    corrected the db error

---
+++ src/main/java/com/example/ezyride/EzyRide/exceptions/TooManyMembersException.java
@@ -0,0 +1,7 @@
+package com.example.ezyride.EzyRide.exceptions;
+
+public class TooManyMembersException extends RuntimeException{
+    public TooManyMembersException(String message) {
+        super(message);
+    }
+}
